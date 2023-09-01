c3f8fb766f574b8109b0435f5e0283f4ddf6c5b4
Bonna Borsoi <bonnagilborsoi@gmail.com>
2023-09-01 19:47:40 -0300

    update dto, added more validation

--- src/modules/purchase/dto/purchase.dto.ts
+++ src/modules/purchase/dto/purchase.dto.ts
@@ -1,15 +1,16 @@
-import { IsNumber, Min, IsNotEmpty} from 'class-validator';
+import { IsNumber, Min, IsNotEmpty } from 'class-validator';
 
 export class PurchaseDTO {
-
+  
   @IsNotEmpty({ message: "Id cannot be empty" })
   @IsNumber()
+  @Min(0, { message: "Id must be greater than or equal to 0" })
   productId: number;
 
   purchaseId?: string;
 
-  @IsNotEmpty({ message: "Quantity cannot be empty" })
+  @IsNotEmpty({ message: 'Quantity cannot be empty' })
   @IsNumber()
-  @Min(1, { message: "Quantity must be greater than or equal to 1" })
+  @Min(1, { message: 'Quantity must be greater than or equal to 1' })
   quantity: number;
 }
